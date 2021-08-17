5997d8254a5312e458fc877b37a1fc16b2a0be2b
spamfreeme2 <41799144+spamfreeme2@users.noreply.github.com>
2021-08-17 22:36:51 +0530

    basic made

---
+++ utility/main.js
@@ -0,0 +1,21 @@
+// Counter for all 
+function animateValue(id, start, end, duration) {
+    if (start === end) return;
+    var range = end - start;
+    var current = start;
+    var increment = end > start? 1 : -2;
+    var stepTime = Math.abs(Math.floor(duration / range));
+    var obj = document.getElementById(id);
+    var timer = setInterval(function() {
+        current += increment;
+        obj.innerHTML = current;
+        if (current == end) {
+            clearInterval(timer);
+        }
+    }, stepTime);
+  }
+  
+  // goal counter call functions
+  animateValue("counter1", 1, 1000,-1000);
+  animateValue("counter2", 1, 50,3000);
+  animateValue("counter3", 1, 10,3000);
