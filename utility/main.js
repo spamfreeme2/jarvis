05bf8691f4d84883480b68e0571ae4d13a6843e5
spamfreeme2 <41799144+spamfreeme2@users.noreply.github.com>
2021-08-20 00:25:19 +0530

    pretty

--- utility/main.js
+++ utility/main.js
@@ -1,20 +1,20 @@
-// Counter for all 
+// Counter for all
 function animateValue(id, start, end, duration) {
-    if (start === end) return;
-    var range = end - start;
-    var current = start;
-    var increment = end > start? 1 : -2;
-    var stepTime = Math.abs(Math.floor(duration / range));
-    var obj = document.getElementById(id);
-    var timer = setInterval(function() {
-        current += increment;
-        obj.innerHTML = current;
-        if (current == end) {
-            clearInterval(timer);
-        }
-    }, stepTime);
-  }
-  // goal counter call functions
-  animateValue("counter1", 1, 1000,-1000);
-  animateValue("counter2", 1, 500,-500);
-  animateValue("counter3", 1, 5,2000);
+  if (start === end) return;
+  var range = end - start;
+  var current = start;
+  var increment = end > start ? 1 : -2;
+  var stepTime = Math.abs(Math.floor(duration / range));
+  var obj = document.getElementById(id);
+  var timer = setInterval(function () {
+    current += increment;
+    obj.innerHTML = current;
+    if (current == end) {
+      clearInterval(timer);
+    }
+  }, stepTime);
+}
+// goal counter call functions
+animateValue("counter1", 1, 1000, -1000);
+animateValue("counter2", 1, 500, -500);
+animateValue("counter3", 1, 5, 2000);
